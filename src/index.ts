f3667f59a3bf082eba391d82c1f0f51a4e422742
neo <arklove@qq.com>
2023-08-12 16:43:09 +0800

    feat: 切换方案

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import parser from 'yargs-parser'
-import {updateNextHosts, updateHosts} from './hosts'
+import {updateNextHosts} from './hosts'
 
 const argv = parser(process.argv.slice(2))
 
-if (argv.type === 'default') {
-  updateHosts()
-} else if (argv.type === 'next') {
-  updateNextHosts()
-}
+updateNextHosts()
